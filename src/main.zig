125b44e71d242488af2b7f5c0ceb51830c9b7e99
Lorenzo Rossi <lorenzorossi.tn@gmail.com>
2025-11-26 15:03:34 +0100

    save

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,17 @@
 const std = @import("std");
 const net = std.Io.net;
 const print = std.debug.print;
+const log = std.log;
 
 const Server = @import("server.zig").Server;
 const Request = @import("request.zig");
 
 pub fn main() !void {
+    log.info("Starting server...", .{});
+
+    const address = try net.IpAddress.parseIp4("127.0.0.1", 8000);
+    log.info("Listening on address {f}", .{address});
+
     var alloc = std.heap.DebugAllocator(.{}){};
     const memory = alloc.allocator();
     defer _ = alloc.deinit();
@@ -14,15 +20,17 @@ pub fn main() !void {
     defer threaded.deinit();
     const io = threaded.io();
 
-    const server = Server.init(io, try net.IpAddress.parseIp4("127.0.0.1", 8000));
-    var listen = try server.listen();
+    var listener = try address.listen(io, .{});
 
-    const connection = try listen.accept(io);
-    defer connection.close(io);
+    while (listener.accept(io)) |conn| {
+        defer conn.close(io);
 
-    var request_buffer: [10000]u8 = undefined;
-    @memset(request_buffer[0..], 0);
-    try Request.read_request(io, connection, request_buffer[0..]);
+        var request_buffer: [8192]u8 = undefined;
+        @memset(request_buffer[0..], 0);
+        try Request.read_request(io, conn, request_buffer[0..]);
 
-    std.debug.print("{s}\n", .{request_buffer});
+        std.debug.print("{s}\n", .{request_buffer});
+    } else |err| {
+        log.err("Error while accepting connection: {any}", .{err});
+    }
 }
