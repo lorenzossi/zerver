4c777a70fe9a164aa406d9f8ce1b3a80a3b0d379
¨lorenzossi¨ <lorenzorossi.tn@gmail.com>
2025-11-27 16:51:03 +0100

    save

--- src/request.zig
+++ src/request.zig
@@ -5,16 +5,14 @@ pub fn read_request(io: std.Io, stream: net.Stream, buffer: []u8) !void {
     var read_buffer: [1]u8 = undefined;
     var reader: net.Stream.Reader = stream.reader(io, &read_buffer);
 
-    const delim = "\r\n\r\n";
+    const delim = "\r\n\r";
 
     var i: usize = 0;
     var value: ?u8 = reader.interface.takeByte() catch null;
     while (value) |not_null_value| : (i += 1) {
-        if (not_null_value == '\r' and i >= 3) {
-            if (std.mem.eql(u8, buffer[(i - 3)..(i + 1)], delim)) {
-                // buffer[i] = not_null_value;
-
-                buffer[i - 1] = '_';
+        if (not_null_value == '\n' and i >= 3) {
+            if (std.mem.eql(u8, buffer[(i - 3)..(i)], delim)) {
+                buffer[i] = not_null_value;
                 break;
             }
         }
